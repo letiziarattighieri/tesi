2825d0e89f5ff6eeb4ddcb4657bccc5fef267863
Letizia Rattighieri <100956798+letiziarattighieri@users.noreply.github.com>
2024-02-19 13:22:13 +0100

    screen_blocchi.r

---
+++ screen_blocchi.r
@@ -0,0 +1,2 @@
+# Qui metti le parti di codice da mettere come screen nella tesi, cambia interfaccia in modo da renderlo chiaro
+# capisci se mettere il 17.02 o il 19.02, hanno lo stesso numero di poligoni ma il 19.02 ha un'area in meno (FR3) perché avevo solo 6 poligoni di fiori
